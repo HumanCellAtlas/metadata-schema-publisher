fc32ad14c53a758a60e40d0a8b1f17b3ca97d4a5
Daniel Vaughan <dan@danielvaughan.com>
2018-02-04 18:27:35 +0000

    added initial git functionality

--- README.md
+++ README.md
@@ -8,10 +8,19 @@ serverless invoke local --function functionName
 ### Simulate a github event
 
 ```
-serverless invoke local --function onGithubEvent --path ./examples/github-event.json
+serverless invoke local --function onGithubEvent --path ./examples/github-event-release.json
+```
+
+### Invoke
+```
+serverless invoke --function onGithubRelease --path ./examples/github-event-release.json --aws-profile secondary-hca-profile
+
 ```
 
 ### Suggested Logic
+On github [release](https://developer.github.com/v3/activity/events/types/#releaseevent) event
+
+AWS SNS is an integration rather than a webhook and it seems to receive only push events as opposed to webhooks where we could filter purely on a release
 
 If a new tag has been created e.g.
 ```
@@ -21,6 +30,7 @@ If a new tag has been created e.g.
 Check out that tag and copy the content of json_schema into a new s3 directory with the same name as the tag.
 
 - Use something like this for do git clone into /tmp https://github.com/nodegit/nodegit
+    - Lambda does not have git installed so try require("lambda-git")(); which installs a git binary on lambda
 - Copy files into S3
 ```
    var s3 = new AWS.S3();
--- handler.js
+++ handler.js
@@ -1,14 +1,80 @@
-'use strict';
+'use strict'
+const Git = require('nodegit')
+const rimraf = require('rimraf')
+const aws = require('aws-sdk')
+const s3 = new aws.S3()
+const fs = require('fs')
 
 module.exports.onGithubEvent = (event, context, callback) => {
-  console.log(JSON.stringify(event))
+  let sns = event.Records[0].Sns
+  let notification = JSON.parse(sns.Message)
+  console.log(notification)
   const response = {
     statusCode: 200,
     body: JSON.stringify({
       message: 'Go Serverless v1.0! Your function executed successfully!',
       input: event,
     }),
-  };
+  }
 
-  callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
-};
+  callback(null, {message: 'Go Serverless v1.0! Your function executed successfully!'})
+}
+
+module.exports.onGithubRelease = (event, context, callback) => {
+  let message = JSON.parse(event.body)
+  let tag_name = message.release.tag_name
+  let html_url = message.repository.html_url
+  clone(tag_name, html_url, callback)
+}
+
+function clone (tag_name, html_url, callback) {
+  require('lambda-git')()
+  let url = html_url
+  let local = '/tmp/repo'
+  let cloneOptions = {}
+  cloneOptions.checkoutBranch = tag_name
+  rimraf(local, function () {
+    console.log('* Removed existing: ' + local)
+    console.log('* Cloning: ' + url + ' tag: ' + tag_name)
+    Git.Clone(url, local).then(function (repo) {
+      console.log('* Cloned: ' + url + 'to ' + local)
+      copyToS3(local, tag_name, callback)
+    }).catch(function (err) {
+      callback(err, null)
+    })
+  })
+}
+
+function copyToS3 (local, tag_name, callback) {
+  const params = {Bucket: 'schema.data.humancellatlas.org', Key: 'tag_name', ACL: 'public-read', Body: ''}
+  upload(params)
+  fs.readdir(local + '/json_schema', (err, files) => {
+    files.forEach(file => {
+      fs.readFile(local + '/json_schema/' + file, function (err, data) {
+        var param = {Bucket: 'schema.data.humancellatlas.org/' + tag_name, Key: file, Body: data}
+        console.log(param)
+      })
+    })
+  })
+  finish(callback)
+}
+
+function upload(params) {
+  s3.upload(params, function (err, data) {
+    if (err) {
+      console.log('Error creating the file: ', err)
+      callback(err, null)
+    } else {
+      console.log('Successfully created a folder on S3')
+      f
+    }
+  })
+}
+
+function finish (callback) {
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({}),
+  }
+  callback(null, response)
+}
