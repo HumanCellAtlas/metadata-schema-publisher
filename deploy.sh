87e737023ae15c7dc7f0a9465f91a4f60430c532
Daniel Vaughan <dvaughan@ebi.ac.uk>
2018-02-05 15:23:37 +0000

    added mapping

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-serverless client deploy --aws-profile secondary-hca-profile
+#serverless client deploy --aws-profile secondary-hca-profile
 serverless deploy --aws-profile secondary-hca-profile
