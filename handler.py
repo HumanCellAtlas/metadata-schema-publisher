ab1d62d406bf68b0276d90bfd7d67418422a0ed5
Daniel Vaughan <dvaughan@ebi.ac.uk>
2018-02-13 21:04:49 +0000

    added git dependency for python

--- handler.py
+++ handler.py
@@ -1,16 +1,34 @@
 import json
+import shutil
+from git import Repo
 
 def on_github_release(event, context):
-    process_event(event)
+    release = process_event(event)
+    local_path = "/tmp/repo"
+    repo = clone_repo(release, local_path)
+    result = {}
+    result["release"] = release
+    result["repo"] = repo
     response = {
         "statusCode": 200,
-        "body": ""
+        "body": result
     }
     return response
     
 def process_event(event):
     message = json.loads(event["body"])
-    print(message)
-    tag_name = message["release"]["tag_name"]
-    html_url = message["repository"]["html_url"]
-    print("tag_name: " + tag_name)
+    release = {}
+    release["tag_name"] = message["release"]["tag_name"]
+    release["repo_url"] = message["repository"]["html_url"]
+    return release
+
+def clone_repo(release, local_path):
+    clear_local_path(local_path)
+    repo = {}
+    repo["remote_url"] = release["repo_url"]
+    repo["local_path"] = local_path
+    Repo.clone_from(repo["remote_url"], local_path)
+    return repo
+    
+def clear_local_path(local_path):
+    shutil.rmtree(local_path)
