c66872c29a3a46ba2819cc8b2325965d327ddd9e
Daniel Vaughan <dvaughan@ebi.ac.uk>
2018-02-14 20:37:15 +0000

    uploads successfully

--- handler.py
+++ handler.py
@@ -8,32 +8,28 @@ from github import Github, GithubException
 
 
 def on_github_release(event, context):
-    repo_name = process_event(event)
+    repo_name = _process_event(event)
     repo = Github("0a43698e4acb763d4713f2e1765a0d7622ab26be").get_repo(repo_name)
-    traverse_repository(repo, 'json_schema')
+    result = _process_directory(repo, 'json_schema')
     response = {
         "statusCode": 200,
-        "body": ""
+        "body": result
     }
     return response
 
 
-def process_event(event):
+def _process_event(event):
     message = json.loads(event["body"])
     return message["repository"]["full_name"]
 
-
-def traverse_repository(repo, server_path):
-    process_directory(repo, server_path)
-
-
-def process_directory(repo, server_path):
+def _process_directory(repo, server_path):
+    created_list = []
     branch = repo.get_branch('v5_prototype')
     sha = branch.commit.sha
     contents = repo.get_dir_contents(server_path, ref=sha)
     for content in contents:
         if content.type == 'dir' and ("bundle" not in content.path):
-            process_directory(repo, content.path)
+            created_list.extend(_process_directory(repo, content.path))
         else:
             try:
                 path = content.path
@@ -41,22 +37,23 @@ def process_directory(repo, server_path):
                 if file_extension == '.json':
                     file_content = repo.get_contents(path, ref=sha)
                     file_data = base64.b64decode(file_content.content)
-                    key = get_schema_key(file_data)
-                    upload(key, file_data)
+                    key = _get_schema_key(file_data)
+                    created = _upload(key, file_data)
+                    if created:
+                        created_list.append(key) 
             except(GithubException, IOError) as e:
                 logging.error('Error processing %s: %s', content.path, e)
+    return created_list
 
 
-def upload(key, file_data):
-    bucket_name = os.environ['BUCKET']
+def _upload(key, file_data):
+    bucket = os.environ['BUCKET']
     s3 = boto3.client('s3')
-    #bucket = s3.Bucket(bucket_name)
-    print("bucket: " + bucket_name + ", key: " + key)
-    if not _key_exists(s3, bucket_name, key):
-        s3.put_object(Bucket=bucket_name, Key=key, Body=file_data)
-        print("- created")
+    if not _key_exists(s3, bucket, key):
+        s3.put_object(Bucket=bucket, Key=key, Body=file_data, ACL='public-read')
+        return True
     else:
-        print("- already exists")
+        return False
 
 
 def _key_exists(s3, bucket, key):
@@ -69,9 +66,9 @@ def _key_exists(s3, bucket, key):
             return obj['Size']
 
 
-def get_schema_key(file_data):
+def _get_schema_key(file_data):
     file_json = json.loads(file_data)
     schema_id = file_json['id']
     key = schema_id.replace(".json", "")
-    key = key.replace("http://schema.humancellatlas.org", "")
+    key = key.replace("http://schema.humancellatlas.org/", "")
     return key
