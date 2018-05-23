b2843476949cc55b15e341d5406c5d1dfb1ca6ab
Daniel Vaughan <dvaughan@ebi.ac.uk>
2018-05-23 15:31:43 +0100

    Refactored to allow for develop as well as master

--- handler.py
+++ handler.py
@@ -8,22 +8,25 @@ from github import Github, GithubException
 
 from release_prep import ReleasePreparation
 
+
 def on_github_push(event, context, dryrun=False):
     message = _process_event(event)
     ref = message["ref"]
-    if ref == "refs/heads/master":
+    if ref == "refs/heads/master" or ref == "refs/heads/develop":
         repo_name = message["repository"]["full_name"]
         pusher = message["pusher"]["name"]
-        api_key = "0a43698e4acb763d4713f2e1765a0d7622ab26be" #os.environ['API_KEY']
+        api_key = os.environ['API_KEY']
         repo = Github(api_key).get_repo(repo_name)
-        notification_message = "Commit to " + ref + " detected on " + repo_name + " by " + pusher
+        branch = repo.get_branch(ref)
+        notification_message = "Commit to " + ref + " detected on " + repo_name + " branch " + branch.name + " by " + pusher
         print(notification_message)
         _send_notification(notification_message, context, dryrun)
-        result = _process_directory(repo, 'json_schema', context, dryrun)
+        server_path = 'json_schema'
+        versions_file = repo.get_contents(server_path + "/versions.json", branch.name)
+        version_numbers = base64.b64decode(versions_file.content)
+        result = _process_directory(repo, branch.name, server_path, version_numbers, context, dryrun)
         result_str = "\n".join(result)
         result_message = ""
-        if dryrun:
-            result_message = "DRY RUN - "
         if len(result) == 0:
             result_message = result_message + "No schema changes published"
         else:
@@ -46,35 +49,30 @@ def _process_event(event):
     return message
 
 
-def _process_directory(repo, server_path, context, dryrun=False):
-    print("Processing " + server_path + " in " + repo.name)
+def _process_directory(repo, branch_name, server_path, version_numbers, context, dryrun=False):
+    print("Processing " + server_path + " in " + branch_name + " branch of " + repo.name)
     created_list = []
-    contents = repo.get_dir_contents(server_path)
-    versions_file = repo.get_contents(server_path + "/versions.json")
-    version_numbers = base64.b64decode(versions_file.content)
+    contents = repo.get_dir_contents(server_path, branch_name)
     for content in contents:
         if content.type == 'dir':
-            created_list.extend(_process_directory(repo, content.path, context, dryrun))
+            created_list.extend(_process_directory(repo, branch_name, content.path, version_numbers, context, dryrun))
         else:
             try:
                 path = content.path
                 file_root, file_extension = os.path.splitext(path)
                 if file_extension == '.json':
                     print("- processing: " + path)
-                    file_content = repo.get_contents(path)
-                    file_data = base64.b64decode(file_content.content)
-                    # TODO: @dan.v: replace context in row below with a variable equal to "dev" for triggers
-                    # from merges into develop or null for merges into master
-                    expanded_file_data = ReleasePreparation.expandURLs(path, content.path, file_data, version_numbers, context)
-
+                    file_content = repo.get_contents(path, branch_name)
+                    data = base64.b64decode(file_content.content)
+                    json_data = json.loads(data.decode('utf8'))
+                    release_preparation = ReleasePreparation()
+                    expanded_file_data = release_preparation.expandURLs(server_path, path, json_data, version_numbers,
+                                                                        branch_name)
                     key = _get_schema_key(expanded_file_data)
                     if key is None:
                         print("- could not find key for: " + path)
                     else:
-                        if dryrun:
-                            created = True
-                        else:
-                            created = _upload(key, file_data, context)
+                        created = _upload(key, branch_name, data, context, dryrun)
                         if created:
                             created_list.append(key)
                 else:
@@ -84,19 +82,25 @@ def _process_directory(repo, server_path, context, dryrun=False):
     return created_list
 
 
-def _upload(key, file_data, context, dryrun=False):
-    bucket = os.environ['BUCKET']
-    s3 = boto3.client('s3')
-    if not _key_exists(s3, bucket, key):
-        try:
-            s3.put_object(Bucket=bucket, Key=key, Body=file_data, ContentType='application/json', ACL='public-read')
-            return True
-        except Exception as e:
-            error_message = 'Error uploading ' + key
-            print(error_message, e)
-            _send_notification(error_message, context, dryrun)
+def _upload(key, branch_name, file_data, context, dryrun=False):
+    if dryrun:
+        return True
     else:
-        return False
+        if branch_name == "develop":
+            bucket = os.environ['DEV_BUCKET']
+        else:
+            bucket = os.environ['PROD_BUCKET']
+        s3 = boto3.client('s3')
+        if not _key_exists(s3, bucket, key):
+            try:
+                s3.put_object(Bucket=bucket, Key=key, Body=file_data, ContentType='application/json', ACL='public-read')
+                return True
+            except Exception as e:
+                error_message = 'Error uploading ' + key
+                print(error_message, e)
+                _send_notification(error_message, context, dryrun)
+        else:
+            return False
 
 
 def _key_exists(s3, bucket, key):
@@ -110,11 +114,11 @@ def _key_exists(s3, bucket, key):
 
 
 def _get_schema_key(file_data):
-    file_json = json.loads(file_data)
-    if 'id' in file_json:
-        schema_id = file_json['id']
-        key = schema_id._replace(".json", "")
-        key = key._replace("https://schema.humancellatlas.org/", "")
+    if 'id' in file_data:
+        schema_id = file_data['id']
+        key = schema_id.replace(".json", "")
+        key = key.replace("https://schema.humancellatlas.org/", "")
+        key = key.replace("https://schema.dev.humancellatlas.org/", "")
     else:
         key = None
     return key
@@ -122,7 +126,7 @@ def _get_schema_key(file_data):
 
 def _send_notification(message, context, dryrun=False):
     if dryrun:
-        print(message)
+        print("DRY RUN:" + message)
     else:
         topic_name = os.environ['TOPIC_NAME']
         account_id = context.invoked_function_arn.split(":")[4]
--- tests/handler_test.py
+++ tests/handler_test.py
@@ -1,3 +1,5 @@
+import os
+
 from unittest import TestCase
 from unittest.mock import patch
 import handler
@@ -6,9 +8,20 @@ import json
 
 class HandlerTest(TestCase):
 
-    def test_process_event(self):
+    def test_process_event_for_master(self):
         # given:
-        with open('files/mock-github-push-event.json') as json_file:
+        os.environ["API_KEY"] = "0a43698e4acb763d4713f2e1765a0d7622ab26be"
+        with open('files/mock-master-github-push-event.json') as json_file:
+            event_json = json.loads(json_file.read())
+
+        # when:
+        response = handler.on_github_push(event_json, None, True)
+        print(response)
+
+    def test_process_event_for_develop(self):
+        # given:
+        os.environ["API_KEY"] = "0a43698e4acb763d4713f2e1765a0d7622ab26be"
+        with open('files/mock-develop-github-push-event.json') as json_file:
             event_json = json.loads(json_file.read())
 
         # when:
