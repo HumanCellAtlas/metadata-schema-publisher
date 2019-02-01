85694bb906c043a0818e702b025b6c3895c1bfac
Alegria Aclan <aaclan@ebi.ac.uk>
2019-02-01 14:18:57 +0000

    Avoid caching

--- handler.py
+++ handler.py
@@ -136,7 +136,7 @@ def _upload(key, branch_name, file_data, context, dryrun=False):
         if not _key_exists(s3, bucket, key):
             try:
                 s3.put_object(Bucket=bucket, Key=key, Body=json.dumps(file_data, indent=2),
-                              ContentType='application/json', ACL='public-read')
+                              ContentType='application/json', ACL='public-read', CacheControl="no-cache")
                 return True
             except Exception as e:
                 error_message = 'Error uploading ' + key
