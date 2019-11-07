5af4de63cf9274f62fbdce7a585094d737897274
Alegria Aclan <aaclan@ebi.ac.uk>
2019-11-07 16:31:49 +0000

    Raise any exceptions when there is a problem getting secrets

--- handler.py
+++ handler.py
@@ -65,7 +65,7 @@ def on_github_push(event, context, dryrun=False):
         repo = Github(access_token).get_repo(repo_name)
         branch = repo.get_branch(ref)
         pusher = message["pusher"]["name"]
-        notification_message = "Commit to " + ref + " detected on " + repo_name + " branch " + branch.name + " by " +\
+        notification_message = "Commit to " + ref + " detected on " + repo_name + " branch " + branch.name + " by " + \
                                pusher
         print(notification_message)
         _send_notification(notification_message, context, dryrun)
@@ -210,7 +210,8 @@ def sns_to_slack(event, context):
     message = sns['Message']
 
     secret_name = os.environ['SECRET_NAME']
-    secrets = json.loads(get_secret(secret_name))
+    secret = get_secret(secret_name)
+    secrets = json.loads(secret)
 
     webhook_url = secrets.get('SLACK_URL')
 
@@ -235,41 +236,13 @@ def get_secret(secret_name, region_name=None):
         region_name=region_name
     )
 
-    # In this sample we only handle the specific exceptions for the 'GetSecretValue' API.
-    # See https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
-    # We rethrow the exception by default.
+    get_secret_value_response = client.get_secret_value(
+        SecretId=secret_name
+    )
 
-    try:
-        get_secret_value_response = client.get_secret_value(
-            SecretId=secret_name
-        )
-    except ClientError as e:
-        if e.response['Error']['Code'] == 'DecryptionFailureException':
-            # Secrets Manager can't decrypt the protected secret text using the provided KMS key.
-            # Deal with the exception here, and/or rethrow at your discretion.
-            raise e
-        elif e.response['Error']['Code'] == 'InternalServiceErrorException':
-            # An error occurred on the server side.
-            # Deal with the exception here, and/or rethrow at your discretion.
-            raise e
-        elif e.response['Error']['Code'] == 'InvalidParameterException':
-            # You provided an invalid value for a parameter.
-            # Deal with the exception here, and/or rethrow at your discretion.
-            raise e
-        elif e.response['Error']['Code'] == 'InvalidRequestException':
-            # You provided a parameter value that is not valid for the current state of the resource.
-            # Deal with the exception here, and/or rethrow at your discretion.
-            raise e
-        elif e.response['Error']['Code'] == 'ResourceNotFoundException':
-            # We can't find the resource that you asked for.
-            # Deal with the exception here, and/or rethrow at your discretion.
-            raise e
+    # Decrypts secret using the associated KMS CMK.
+    # Depending on whether the secret is a string or binary, one of these fields will be populated.
+    if 'SecretString' in get_secret_value_response:
+        return get_secret_value_response['SecretString']
     else:
-        # Decrypts secret using the associated KMS CMK.
-        # Depending on whether the secret is a string or binary, one of these fields will be populated.
-        if 'SecretString' in get_secret_value_response:
-            secret = get_secret_value_response['SecretString']
-        else:
-            decoded_binary_secret = base64.b64decode(get_secret_value_response['SecretBinary'])
-
-    return secret
+        return base64.b64decode(get_secret_value_response['SecretBinary'])
